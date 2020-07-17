16a738ca245ce6a78f4c6caca95110e268c7ec08
Ludwigm6 <marvin.ludwig93@gmail.com>
2020-07-17 14:58:29 +0200

    interface template

--- msInterface.py
+++ msInterface.py
@@ -5,10 +5,25 @@
 import Metashape
 from msFunctions.msSubsetImages import subsetImages
 from msFunctions.msError import exportMarker
+from msFunctions.msExportTiepointError import *
 
 
-def menuSubsetImages():
-	subsetImages()
+
+
+# helper for optional all chunk processing:
+def menuHelper(fun):
+	def menuFunc():
+		ac = Metashape.app.getBool("Process all chunks?")
+		if ac:
+			for chunk in Metashape.app.document.chunks:
+				fun(chunk)
+		else:
+			fun(chunk)
+	return menuFunc
+	
+
+
+menuSubsetImages = menuHelper(subsetImages)
 
 
 def menuError():
@@ -21,10 +36,20 @@ def menuError():
 	
 
 
+def menuTiepoints():
+	ac = Metashape.app.getBool("Process all chunks?")
+	if ac:
+		for chunk in Metashape.app.document.chunks:
+			ExportTiepointError(chunk)
+	else:
+		ExportTiepointError(chunk)
+
+
+
 
 Metashape.app.addMenuItem("MetashapeTools/Subset Images", menuSubsetImages)
 Metashape.app.addMenuItem("MetashapeTools/Export Marker Error", menuError)
-
+Metashape.app.addMenuItem("MetashapeTools/Export Tiepoint Error", menuTiepoints)
 
 
 
